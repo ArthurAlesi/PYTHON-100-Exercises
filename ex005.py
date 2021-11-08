0186eff4f050495957bf87ffdb6b91c1e05238b4
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2021-11-08 11:10:00 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -2,5 +2,5 @@ num = int(input("informe um numero"))
 anter = num -1
 suces = num + 1
 print("num: ", num)
-print("antecessor: ", anter)
-print("sucessor: ", suces)
+print("antecessor de ",num,": " ,anter)
+print("sucessor ",num,": " ,suces)
