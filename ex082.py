71b63f703768691e995bcce500bbc3146c89afff
Arthur Alesi Souza <54421573+ArthurAlesi@users.noreply.github.com>
2021-12-01 13:56:55 -0300

    ex 81 - 85

---
+++ ex082.py
@@ -0,0 +1,25 @@
+'''
+crie um programa que vai legar varios numeros e colocar em uma lista.
+Depois disso crie duas listas e extras que vao conter apenas os
+ valores pares e os valores impares digitados respectivamente.
+ ao final mostre o conteudo das 3 listas geradas
+'''
+
+num = []
+pares = []
+impares = []
+while True:
+    num.append(int(input("Digite um numero: ")))
+    continuar = str(input("Deseja continuar [S/N]"))
+    if continuar in 'nN':
+        break
+
+for i, v in enumerate(num):
+    if v% 2 ==0:
+        pares.append(v)
+    else:
+        impares.append(v)
+
+print(num)
+print(pares)
+print(impares)
---
+++ ex085.py
@@ -0,0 +1,15 @@
+'''
+Crie um programa onde o usuario possa digitar sete valores numericos e dacastre-os em
+uma lista unica que mantenha separados os valores pares e impares. No final, mnostre
+os valores pares e imapres em ordem crescente
+'''
+
+num = [[], []]
+valor = 0
+for c in range(1,8):
+    valor = int(input(f"Digite o  {c}valor:"))
+    if valor % 2 == 0:
+        num[0].append(valor)
+    else:
+        num[1].append(valor)
+
